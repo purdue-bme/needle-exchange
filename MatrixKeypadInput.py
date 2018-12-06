61498067275f98bc09be35c3088ce323f62c05cc
Madi Rogers <31542088+madirogers@users.noreply.github.com>
2018-12-05 21:51:01 -0500

    [INCOMPLETE] Update MatrixKeypadInput.py

--- MatrixKeypadInput.py
+++ MatrixKeypadInput.py
@@ -1,27 +1,50 @@
-#include "Arduino.h"
-#include "Keypad.h"
+"""
+RPi.GPIO pins needed:
+Pi GPIO 5 to keypad pin 1
+Pi GPIO 6 to keypad pin 2
+Pi GPIO 13 to keypad pin 3
+Pi GPIO 19 to keypad pin 4
+Pi GPIO 26 to keypad pin 5
+Pi GPIO 16 to keypad pin 6
+Pi GPIO 20 to keypad pin 7
+Pi GPIO 21 to keypad pin 8
+"""
 
-const byte ROWS = 4; //four rows
-const byte COLS = 4; //four columns
-char keys[ROWS][COLS] = {
-  {'1','2','3','A'},
-  {'4','5','6','B'},
-  {'7','8','9','C'},
-  {'*','0','#','D'}
-};
-byte rowPins[ROWS] = {A3, A2, A1, A0}; //connect to the row pinouts of the keypad - four in a row
-byte colPins[COLS] = {MOSI, SCK, A5, A4}; //connect to the column pinouts of the keypad - four in a row
+# code suggested to access RPi GPIO pins:
+# https://www.dummies.com/computers/raspberry-pi/use-python-access-gpio-pins-raspberry-pi/
 
-Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+# code suggested using CIrcuitPython installation of Matrix Keypad Library:
+# https://learn.adafruit.com/matrix-keypad/python-circuitpython
 
-void setup() {
-  Serial.begin(9600);
-}
+#!/usr/bin/python3
+import time
+import digitalio
+import RPi.GPIO as GPIO #import RPi.GPIO as GPIO
+import adafruit_matrixkeypad as amk #import Adafruit Matrix Keypad Library as amk
 
-void loop() {
-  char key = keypad.getKey();
+5, 6, 13, 19, 26, 16, 20, 21
 
-  if (key != NO_KEY){
-    Serial.println(key);
-  }
-}
+cols = [GPIO.setup(x,GPIO.IN) for x in (13, 5, 26)]
+crows = [GPIO.setup(x,GPIO.IN) for x in (6, 21
+
+"""
+import time
+import digitalio
+import board
+import adafruit_matrixkeypad
+
+cols = [digitalio.DigitalInOut(x) for x in (board.D11, board.D13, board.D9)]
+rows = [digitalio.DigitalInOut(x) for x in (board.D12, board.D5, board.D6, board.D10)]
+keys = ((1, 2, 3, 'A'),
+        (4, 5, 6, 'B'),
+        (7, 8, 9, 'C'),
+        ('*', 0, '#', 'D'))
+
+keypad = adafruit_matrxikeypad.MatrixKeypad(rows, cols, keys)
+
+while True:
+  keys = keypad.presed_keys
+  if keys:
+    print("Press: ", keys)
+    time.sleep(0.1)
+"""
